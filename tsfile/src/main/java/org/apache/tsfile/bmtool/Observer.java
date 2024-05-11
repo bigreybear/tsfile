c73ed9c8243e7f929807e69a181a7736de25fdaf
zhaoxin <xzhaothss@gmail.com>
2024-05-11 22:48:47 +0800

    ready for include tailored parquet

--- tsfile/src/main/java/org/apache/tsfile/bmtool/Observer.java
+++ tsfile/src/main/java/org/apache/tsfile/bmtool/Observer.java
@@ -1,4 +1,10 @@
 package org.apache.tsfile.bmtool;
+import java.io.IOException;
+
+import org.apache.parquet.example.data.Group;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.tsfile.exception.write.WriteProcessException;
+import org.apache.parquet.bmtool.BMWriter;
 
 public class Observer {
   public static long timeRecord = 0;
@@ -9,5 +15,9 @@ public class Observer {
     }
     long laps = System.nanoTime() - timeRecord;
     System.out.println(String.format("%s for %d nano seconds", mark, laps));
+  }
+
+  public static void main(String[] args) throws IOException, WriteProcessException {
+    BMWriter.main(args);
   }
 }
