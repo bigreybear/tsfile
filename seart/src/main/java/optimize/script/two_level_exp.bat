58500d4e2f9bbc1924ddefd99cae5803a077e2be
zhaoxin <xzhao@apache.org>
2024-12-30 00:05:42 +0800

    update scripts

--- seart/src/main/java/optimize/script/two_level_exp.bat
+++ seart/src/main/java/optimize/script/two_level_exp.bat
@@ -12,6 +12,8 @@ set ds[1]=sw
 set ds[2]=zy
 set ds[3]=xyzc
 
+set "vmarg=--add-opens java.base/java.util=ALL-UNNAMED"
+
 set "mm[0]= -mt hash "
 set "mm[1]= -mt hash -ms simple -merge "
 set "mm[2]= -mt hash -ms partial -merge "
@@ -26,9 +28,9 @@ set filename=resultv1229.txt
 for /L %%i in (0,1,3) do (
     echo ============= SPACE for !ds[%%i]! ========= >> %filename%
     for /L %%j in (0,1,7) do (
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -space -depth >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -space -depth >> %filename% 2>&1
         echo. >> %filename%
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -space -twoLevel -depth >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -space -twoLevel -depth >> %filename% 2>&1
         echo. >> %filename%
     )
     echo. >> %filename%
@@ -38,17 +40,17 @@ for /L %%i in (0,1,3) do (
 for /L %%i in (0,1,3) do (
     echo ============= LATENCY for !ds[%%i]! ========= >> %filename%
     for /L %%j in (0,1,7) do (
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -latency >> %filename% 2>&1
         echo. >> %filename%
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency  >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
-        java -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency  >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
+        java !vmarg! -jar seart-1.0.1-SNAPSHOT.jar !mm[%%j]! -ds !ds[%%i]! -twoLevel -latency >> %filename% 2>&1
         echo. >> %filename%
     )
     echo. >> %filename%
